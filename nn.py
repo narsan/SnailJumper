2f9afa475c2a5b3f94b84aeeae3cdde5468d1c2b
Narges Sodeifi <46598182+narsan@users.noreply.github.com>
2022-01-26 22:05:20 +0330

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from ctypes.wintypes import PFILETIME
+from typing import Counter
+
 
 class NeuralNetwork:
 
@@ -10,17 +13,21 @@ class NeuralNetwork:
         :param layer_sizes: A list containing neuron numbers in each layers. For example [3, 10, 2] means that there are
         3 neurons in the input layer, 10 neurons in the hidden layer, and 2 neurons in the output layer.
         """
-        # TODO (Implement FCNNs architecture here)
-        pass
+        # layer_sizes example: [3, 10, 2]
+
+        self.input_layer_size, self.hidden_layer_size, self.output_layer_size = layer_sizes
+
+        # WEIGHTS
+        self.W1 = np.random.normal(size=(self.hidden_layer_size, self.input_layer_size))
+        self.W2 = np.random.normal(size=(self.output_layer_size, self.hidden_layer_size))
+
+        # BIASES
+        self.b1 = np.zeros((self.hidden_layer_size, 1))
+        self.b2 = np.zeros((self.output_layer_size, 1))
 
     def activation(self, x):
-        """
-        The activation function of our neural network, e.g., Sigmoid, ReLU.
-        :param x: Vector of a layer in our network.
-        :return: Vector after applying activation function.
-        """
-        # TODO (Implement activation function here)
-        pass
+        # sigmoid function
+        return 1 / (1 + np.exp(-x))
 
     def forward(self, x):
         """
@@ -28,5 +35,8 @@ class NeuralNetwork:
         :param x: Input vector which is a numpy array.
         :return: Output vector
         """
-        # TODO (Implement forward function here)
-        pass
+        x = x.reshape((self.input_layer_size, 1))
+        a1 = self.activation(self.W1 @ x + self.b1)
+        a2 = self.activation(self.W2 @ a1 + self.b2)
+
+        return a2
